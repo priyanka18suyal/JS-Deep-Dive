f23374d2dfb2803bd0c12f1889207806d0b95647
Priyanka Suyal <127640448+priyanka18suyal@users.noreply.github.com>
2025-12-14 22:41:53 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,18 @@
+// how to declare variables and constant
+const accountId = 12345;
+let accountEmail = "priyanka@gmail.com"
+var accountPassword = "12345"
+accountCity=  "Uttrakhand"
+
+// lets change the value of let and var
+//  accountId = 2
+// console.log(accountId)
+
+accountEmail = "hi"
+console.log(accountEmail)
+
+accountPassword = "6789"
+console.log(accountPassword)
+
+accountCity = "New York"
+console.log(accountCity)
