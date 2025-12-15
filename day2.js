159aa95f2f53f3cf64ca4612598ff0ce52148473
Priyanka Suyal <127640448+priyanka18suyal@users.noreply.github.com>
2025-12-15 04:57:46 +0000

    Add variable declarations and examples in day2.js

--- day2.js
+++ day2.js
@@ -3,10 +3,14 @@ const accountId = 12345;
 let accountEmail = "priyanka@gmail.com"
 var accountPassword = "12345"
 accountCity=  "Uttrakhand"
+//without variable but it should not use but this is also the method
+let accountState; //this is undefined
+
 
 // lets change the value of let and var
 //  accountId = 2
 // console.log(accountId)
+//cannot change const
 
 accountEmail = "hi"
 console.log(accountEmail)
@@ -16,3 +20,11 @@ console.log(accountPassword)
 
 accountCity = "New York"
 console.log(accountCity)
+
+//this is one of the method to print all values at a single time
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functonal scope
+*/
